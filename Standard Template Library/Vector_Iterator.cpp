b9fbee0771ba0ceb30840aa530d2b6a38c36b0a7
Manasi Wader <manasiwader@gmail.com>
2020-06-28 20:10:47 +0530

    Front Back Iterator

---
+++ Standard Template Library/Vector_Iterator.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+
+void iter(vector<int>::iterator it1,vector<int>::iterator it2)
+{
+    vector<int>::iterator it;
+    for(it=it1;it<it2;it++)
+        cout<<*it<<" ";
+    cout<<endl;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v;
+    for(int i=0;i<n;i++)
+    {
+        int val;
+        cin>>val;
+        v.push_back(val);
+    }
+    iter(v.begin(),v.end());
+    return 0;
+}
