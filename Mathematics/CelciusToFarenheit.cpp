14dbaaba733860bc528b5737ba48e5cb6feb9737
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2020-08-07 16:56:28 +0530

    added

---
+++ Mathematics/CelciusToFarenheit.cpp
@@ -0,0 +1,8 @@
+//solution:https://practice.geeksforgeeks.org/problems/convert-celsius-to-fahrenheit/1/?track=ppc-mathematics&batchId=221
+
+//Given a temperature in celsius C. You need to convert the given temperature to Fahrenheit.
+
+double cToF(int C)
+{
+    return (1.8*C)+32;
+}
