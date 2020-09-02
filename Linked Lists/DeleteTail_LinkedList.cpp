426768b66eaabccc88ca654bc2e9d0e695206c33
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2020-09-02 06:14:43 +0530

    deleting last element from linkedlist

---
+++ Linked Lists/DeleteTail_LinkedList.cpp
@@ -0,0 +1,15 @@
+//Given a linked list of size n, you have to delete the tail (last element) in the linked list.
+
+
+Node* deleteTail(Node *head)
+{
+    if(head==NULL)
+    return head;
+    Node* temp=head;
+    while(temp->next->next!=NULL)
+    temp=temp->next;
+    delete temp->next;
+    temp->next=NULL;
+    
+    return head;
+}
