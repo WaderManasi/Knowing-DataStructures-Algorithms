4c9d081290f4d609cb59b61d0005a441ae87dd6a
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2020-09-04 16:25:02 +0530

    delete at pos circular ll

---
+++ Linked Lists/DeleteAtPos_Circularll.cpp
@@ -0,0 +1,22 @@
+Node * deleteAtPosition(Node *head,int pos)
+{
+if(pos==1)
+{
+if(head->next==head)
+return NULL;
+Node *temp=head->next;
+head->next=head->next->next;
+delete(temp);
+return head;
+}
+else
+{
+Node *temp=head;
+Node *cur;
+for(int i=0;i<pos-2;i++) temp=temp->next;
+cur=temp->next;
+temp->next=temp->next->next;
+delete(cur);
+return head;
+}
+}
