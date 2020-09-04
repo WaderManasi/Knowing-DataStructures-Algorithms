57547fc811e08c46e79f19afa5d24c10826b809b
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2020-09-04 16:26:11 +0530

    delete at pos circular ll

--- Linked Lists/DeleteAtPos_Circularll.cpp
+++ Linked Lists/DeleteAtPos_Circularll.cpp
@@ -1,6 +1,7 @@
 //Given a linked list of size n, you have to delete the node at position pos of the linked list and return the new head. The position of initial node is 1.
 //The tail of the circular linked list is connected to the head using next pointer.
 
+//solution
 Node * deleteAtPosition(Node *head,int pos)
 {
 if(pos==1)
