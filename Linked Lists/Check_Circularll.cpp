e4693b9d908f0ba27a4fc3afd3565bdcddab611b
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2020-09-04 08:56:27 +0530

    check if circular ll

---
+++ Linked Lists/Check_Circularll.cpp
@@ -0,0 +1,9 @@
+bool isCircular(Node *head)
+{
+   if(head==NULL || head->next==head) return true;
+   Node* temp=head;
+   while(temp->next!=NULL && temp->next!=head)
+   temp=temp->next;
+   
+   return (temp->next==head)?true:false;
+}
