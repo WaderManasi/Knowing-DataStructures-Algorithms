459c786f2c861164f2ef89c52e025bbd30b7c08a
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2020-09-04 23:21:39 +0530

    delete head doubly linked list

--- Linked Lists/DeleteHead_Doublyll.cpp
+++ Linked Lists/DeleteHead_Doublyll.cpp
@@ -1,3 +1,6 @@
+//Given a doubly linked list of size n, you have to delete the head of the linked list and return the new head.
+//Note: Please set the previous of new head to null, and set the next of old head to null, and then delete the old head.
+
 Node *deleteHead(Node * head)
 {
    if(head==NULL) return NULL;
