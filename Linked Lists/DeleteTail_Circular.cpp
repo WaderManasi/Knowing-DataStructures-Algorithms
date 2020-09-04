068bff60d168cd412c144d00d89158705b2a5be2
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2020-09-04 09:45:41 +0530

    delete at tail circular ll

--- Linked Lists/DeleteTail_Circular.cpp
+++ Linked Lists/DeleteTail_Circular.cpp
@@ -1,6 +1,7 @@
 //Given a circular linked list of size n, you have to delete the tail (last element) in the linked list.
 //In a circular linked list, the tail is connect to the head using the next pointer.
 
+//solution
 Node * deleteTail(Node * head)
 {
     if(head==NULL)  return NULL;
