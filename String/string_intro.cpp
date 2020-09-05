cc0e7a93d51181a723cc7025fc482f8f20f4c404
Manasi Wader <manasiwader@gmail.com>
2020-09-05 15:36:10 +0530

    string intro

--- String/string_intro.cpp
+++ String/string_intro.cpp
@@ -4,5 +4,17 @@ using namespace std;
 int main()
 {
     char str[]="comp";
+    cout<<"\nDisplaying string: ";
+    cout<<str;
+    cout<<"\nSize of str: ";
+    cout<<sizeof(str);
+    /*char str1[]={'c','o','m','k'};
+    cout<<"\nChar array: "; 
+    cout<<str1;*/
+    string name,fname;
+    cout<<"Enter full name: ";
+    cin>>name;
     
+    
+    cout<<endl;
 }
