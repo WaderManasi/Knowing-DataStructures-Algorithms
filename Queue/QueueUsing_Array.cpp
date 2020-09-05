a661e760492e6fa0498296408a0bda806e9e2906
Manasi Wader <manasiwader@gmail.com>
2020-09-05 20:06:43 +0530

    Queue using array

---
+++ Queue/QueueUsing_Array.cpp
@@ -0,0 +1,21 @@
+/* The method push to push element into the queue */
+void MyQueue :: push(int x)
+{
+    arr[rear]=x;
+    rear++;
+}
+
+/*The method pop which return the element 
+  poped out of the queue*/
+int MyQueue :: pop()
+{
+   //
+   if(front==rear)
+    {
+        front=rear=0;
+        return -1;
+    }
+    int a=arr[front];
+    front++;
+    return a;
+}
