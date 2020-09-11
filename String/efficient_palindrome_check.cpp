3f265d7559a30b4bbe0b63b834a87705b697cd4e
Manasi Wader <manasiwader@gmail.com>
2020-09-11 16:06:34 +0530

    palindrome checking in string

---
+++ String/efficient_palindrome_check.cpp
@@ -0,0 +1,30 @@
+//solution: palindrome check
+//In c++ string is mutable
+//Efficient method
+//Auxilary space and time: O(1) :best case
+//                          O(n) :worst case
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+
+       
+bool checkPalin(string &str)
+{
+    string rev=str;
+    reverse(rev.begin(),rev.end());
+    return rev==str?true:false;
+}
+
+int main()
+{
+    string str;
+    getline(cin,str);
+    
+    //Naives method
+    if(checkPalin(str))
+         cout<<"\nString is palindrome\n";
+    else
+        cout<<"\nNot a palindrome\n";
+
+    return 0;
+}
