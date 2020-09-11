a6430fb207fa0c08c91185f29c823a15c37328a0
Manasi Wader <manasiwader@gmail.com>
2020-09-11 15:55:01 +0530

    palindrome checking in string

---
+++ String/palindrome_check.cpp
@@ -0,0 +1,27 @@
+//solution: palindrome check
+
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+
+       
+bool checkPalin(string &str)
+{
+    string rev=str;
+    reverse(rev.begin(),rev.end());
+    return rev==str?true:false;
+}
+
+int main()
+{
+    string str;
+    getline(cin,str);
+    
+    //Naives method
+    if(checkPalin(str))
+         cout<<"\nString is palindrome\n";
+    else
+        cout<<"\nNot a palindrome\n";
+
+    return 0;
+}
