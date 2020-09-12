d5482bc57475f9f4c2ce07c7bee12db953a34498
Manasi Wader <manasiwader@gmail.com>
2020-09-12 18:18:47 +0530

    add

--- String/string_intro.cpp
+++ String/string_intro.cpp
@@ -7,16 +7,16 @@ int main()
     cout<<"\nDisplaying string: ";
     cout<<str;
     cout<<"\nSize of str: ";
-    cout<<sizeof(str);
-    /*char str1[]={'c','o','m','k'};
-    cout<<"\nChar array: "; 
-    cout<<str1;*/
+    cout<<sizeof(str);          //prints size one more than original size 
+  
+  //to input entire string from user
     string name,fname;
     cout<<"\nEnter full name: ";
     
     getline(cin,fname);
     cout<<fname;
     
+    //input entire sentence is not dispalyed
     cout<<"\nEnter full name: ";
     cin>>name;
     cout<<name;
