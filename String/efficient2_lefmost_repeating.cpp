6fccbff5afc2d9d4faae7bb2306f8b9637d49831
Manasi Wader <manasiwader@gmail.com>
2020-09-12 12:36:19 +0530

    add

---
+++ String/efficient2_lefmost_repeating.cpp
@@ -0,0 +1,34 @@
+//Find left most repaeting character
+//solution: 
+//efficient approach:-2 require single traversing time O(n+constant)
+
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+
+int leftRepeat(string str){
+    int count[256];
+    int i;
+    int res=INT_MAX;
+    fill(count,count+256,-1);
+    //initialize array with -1
+    for(i=0;i<str.length();i++)
+    {
+        //fi = first index
+        int fi=count[str[i]];
+        if(fi==-1)
+            count[str[i]]=i;
+        else
+            res = min(res,fi);
+    }
+    return (res==INT_MAX)?-1:res;
+}
+int main()
+{
+    string str;
+    cin.ignore();
+    getline(cin,str);
+    
+    cout<<leftRepeat(str)<<endl;
+    return 0;
+}
