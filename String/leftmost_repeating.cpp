9199677ae2fa176b1fb7aa4214c5e555f39b7497
Manasi Wader <manasiwader@gmail.com>
2020-09-12 11:49:30 +0530

    add

---
+++ String/leftmost_repeating.cpp
@@ -0,0 +1,31 @@
+//Find left most repaeting character
+//solution: 
+
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+
+int leftRepeat(string str){
+     int count[26]={1};
+     for(int i=0;i<str.length();i++)
+     {
+        count[str[i]-'a']++;
+     }
+     for(int i=0;i<26;i++)
+     {
+         if(count[i]>1)
+         {
+            return i;
+         }
+     }
+     return -1;
+}
+int main()
+{
+    string str;
+    cin.ignore();
+    getline(cin,str);
+    
+    cout<<leftRepeat(str)<<endl;
+    return 0;
+}
