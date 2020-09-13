aeb4ae4ccc6282b0c6ccdc1c2696959037039fa7
Manasi Wader <manasiwader@gmail.com>
2020-09-13 18:19:32 +0530

    modified string

--- String/modified_string.cpp
+++ String/modified_string.cpp
@@ -1,5 +1,4 @@
 //Ishaan is playing with strings these days. He has found a new string. He wants to modify it as per the following rules to make it valid:
-
 //The string should not have three consecutive same characters (Refer example for explanation).
 //He can add any number of characters anywhere in the string. 
 //Find the minimum number of characters which Ishaan must insert in the string to make it valid.
