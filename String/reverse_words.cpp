be59ad46385bd1df70bba1fd3774682418f1fcbf
Manasi Wader <manasiwader@gmail.com>
2020-09-13 22:57:19 +0530

    reverse words in string

---
+++ String/reverse_words.cpp
@@ -0,0 +1,44 @@
+//You are given a string s. You need to reverse the string.
+//solution: 
+
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+
+string reverseWords(string s) 
+{ 
+    stack<char>st;
+    string news="";
+    int i=s.length()-1;
+    while(i>=0){
+        if(s[i]!='.')
+        {
+            st.push(s[i]);
+        }
+        else {
+       while(!st.empty())
+            {
+                char ch=st.top();
+                news.push_back(ch);
+                st.pop();
+            }
+            news.push_back('.');
+        }
+        i--;
+    }
+    while(!st.empty())
+            {
+                char ch=st.top();
+                news.push_back(ch);
+                st.pop();
+            }
+    return news;
+} 
+int main()
+{
+    string str;
+    cin>>str;
+    
+    cout<<reverseWords(str)<<endl;
+    return 0;
+}
