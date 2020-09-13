968abfee6156b384a4d17dd55fda03c73c6b1687
Manasi Wader <manasiwader@gmail.com>
2020-09-13 18:03:07 +0530

    modified string

--- String/modified_string.cpp
+++ String/modified_string.cpp
@@ -1,3 +1,9 @@
+//Ishaan is playing with strings these days. He has found a new string. He wants to modify it as per the following rules to make it valid:
+
+//The string should not have three consecutive same characters (Refer example for explanation).
+//He can add any number of characters anywhere in the string. 
+//Find the minimum number of characters which Ishaan must insert in the string to make it valid.
+
 int modified (string a){
     int count=0;
     for(int i=0;i<a.length()-1;i++)
