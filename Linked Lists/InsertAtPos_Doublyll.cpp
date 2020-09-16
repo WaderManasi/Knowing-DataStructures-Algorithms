c16c09aba5f94bf67322c4471451a7bb28aba04c
Manasi Wader <manasiwader@gmail.com>
2020-09-16 17:53:53 +0530

    insert at given pos in doublyy linked list

--- Linked Lists/InsertAtPos_Doublyll.cpp
+++ Linked Lists/InsertAtPos_Doublyll.cpp
@@ -1,3 +1,5 @@
+//Given a doubly-linked list, a position p, and an integer x. The task is to add a new node with value x at the position just after pth node in the doubly linked list.
+
 void addNode(Node *head, int pos, int data)
 {
    Node* newn= new Node(data);
