5486ff305b7511045872670c3d93911c7cb52296
Manasi Wader <manasiwader@gmail.com>
2020-09-16 16:38:54 +0530

    detect loop in a linked list

---
+++ Linked Lists/DetectLoop_Singlyll.cpp
@@ -0,0 +1,17 @@
+bool has_cycle(Node* head) {
+    if(head==NULL) return false;
+    Node* fast=head->next;
+    Node* slow=head;
+    int flag=0;
+    while(fast && fast->next && slow)
+    {
+        if(slow==fast)
+           {
+            flag=1;
+            break;
+        }
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return flag?true:false;
+}
