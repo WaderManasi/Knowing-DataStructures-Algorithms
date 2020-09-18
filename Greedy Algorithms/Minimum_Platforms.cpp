5db62b34954541b650cded0317f341f8a3c9a6ed
Manasi Wader <manasiwader@gmail.com>
2020-09-18 20:28:31 +0530

    Find min no. of platforms

---
+++ Greedy Algorithms/Minimum_Platforms.cpp
@@ -0,0 +1,6 @@
+/*
+Given arrival and departure times of all trains that reach a railway station. Your task is to find the minimum number of platforms required for the railway station so that no train waits.
+Note: Consider that all the trains arrive on the same day and leave on the same day. Also, arrival and departure times will not be same for a train, but we can have arrival time of one train equal to departure of the other.
+In such cases, we need different platforms, i.e at any given instance of time, same platform can not be used for both departure of a train and arrival of another.
+*/
+
