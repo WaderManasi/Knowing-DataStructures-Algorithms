c8749e6936dc68a4bf20500d0cef7234945e8b8e
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2020-09-20 21:45:22 +0530

    Update CheckNthBit.cpp

--- Bit_Magic/CheckNthBit.cpp
+++ Bit_Magic/CheckNthBit.cpp
@@ -1,5 +1,4 @@
 //Given a number N and a bit number K, check if Kth bit of N is set or not. A bit is called set if it is 1. Position of set bit '1' should be indexed starting with 0 from LSB side in binary representation of the number. 
-
 //User function Template for C++
 
 // Function to check if Kth bit is set or not
