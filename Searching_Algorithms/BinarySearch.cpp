f30bb6a45f742302281ff46ead1b54190cff26d3
Manasi Wader <manasiwader@gmail.com>
2021-08-23 16:33:38 +0530

    Binary Search

--- Searching_Algorithms/BinarySearch.cpp
+++ Searching_Algorithms/BinarySearch.cpp
@@ -1,16 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int binarySearch(int arr[],int n)
+int binarySearch(int arr[],int n, int key)
 {
-    
+    int s=0, e=0, mid=0;
+    mid=n/2;
+
+    if(arr[mid]==key)
+    return n/2;
+    else if(key < arr[mid]){
+        e=mid-1;
+    }
+    else{
+        s=mid+1;
+    }
+    // element not found
+    if(s>e)
+    return -1;
 }
 int main()
 {
     int arr[] = {2,4,6,9,11,12,14,20,36,48};
     int target;
     cin>>target;
-    cout<<"Element found at position: "<<binarySearch(arr, 10);
+    cout<<"Element found at position: "<<binarySearch(arr, 10,target);
     
     return 0;
 }
