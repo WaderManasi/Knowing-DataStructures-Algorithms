63f6e2cb044f607e946a14e07d66c9d75442553e
Manasi Wader <manasiwader@gmail.com>
2021-08-23 17:19:05 +0530

    ORDER AGNOSTIC Binary Search

--- Searching_Algorithms/BinarySearch.cpp
+++ Searching_Algorithms/BinarySearch.cpp
@@ -4,7 +4,11 @@ using namespace std;
 int binarySearch(int arr[],int n, int key)
 {
     int s=0, e=n-1, mid=0;
-    while(s<=e){
+    //check wheather array is sorted in ascending order or descending order
+    
+    if(arr[0]<arr[n-1]){
+        //ascending sort
+        while(s<=e){
         mid=s+(e-s)/2;
         if(key < arr[mid]){
             e=mid-1;
@@ -19,13 +23,45 @@ int binarySearch(int arr[],int n, int key)
     }
     // element not found
     return -1;
+    }
+    else if(arr[0]>arr[n-1])
+    {
+        //descending sort
+        while(s<=e){
+        mid=s+(e-s)/2;
+        if(key > arr[mid]){
+            e=mid-1;
+                }
+        else if(key < arr[mid]){
+            s=mid+1;
+            }
+            else
+            {
+                return mid;
+            }   
+    }
+    // element not found
+    return -1;
+    }
+    else
+    {
+        //all array elements are equal
+        if(key==arr[0])
+        return 0;
+        else
+        return -1;
+    }
 }
 int main()
 {
-    int arr[] = {2,4,6,9,11,12,14,20,36,48};
+    int n;    
+    cin>>n;
+    int arr[n];
     int target;
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
     cin>>target;
-    cout<<"Element found at position: "<<binarySearch(arr, 10,target);
+    cout<<"Element found at position: "<<binarySearch(arr,n,target);
     
     return 0;
 }
