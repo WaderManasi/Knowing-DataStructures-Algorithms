2bc110c205de2391ed869a00dff753117d63d664
Manasi Wader <manasiwader@gmail.com>
2021-08-24 16:17:13 +0530

    Ques. 744

---
+++ Searching_Algorithms/LeetCode#744.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int mid=0,s=0,e=letters.size()-1;
+        while(s<=e)
+        {
+            mid=s+(e-s)/2;
+            if(target<letters[mid])
+            {
+                e=mid-1;
+            }
+            else
+            {
+                s=mid+1;
+            }
+        }
+        return letters[s%letters.size()];
+    }
+};
