3af550c17dba53bc0acb5ecb8ec623d20f3a76c2
Manasi Wader <manasiwader@gmail.com>
2021-08-24 22:16:27 +0530

    Ques. 162

---
+++ Searching_Algorithms/LeetCode#162.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int mid=0,s=0,e=nums.size()-1;
+        int flag=0;
+        while(s<e)
+        {
+            mid=s+(e-s)/2;
+            if(nums[mid]>nums[mid+1])
+            {
+                //flag=1;
+                e=mid;
+                //break;
+                
+            }
+            else
+            {
+                s=mid+1;
+            }
+        }
+        return s;
+    }
+};
