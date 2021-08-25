8195d8ef912e49e3efbdba5cc7f793dc38de70b1
Manasi Wader <manasiwader@gmail.com>
2021-08-25 20:59:30 +0530

    Q:852 Find index of peak element in mountain array

--- Searching_Algorithms/Leetcode#852.cpp
+++ Searching_Algorithms/Leetcode#852.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int n=arr.size();
-        int mid=0,s=0,e=n-1,flag=0;
+        int mid=0, s=0, e=nums.size()-1;
         while(s<e){
             mid=s+(e-s)/2;
-        if(arr[mid]>arr[mid+1])
-        {
-            //here there may lie the ans;
-            e=mid;
+            if(nums[mid]>nums[mid+1])   e=mid;
+            else    s=mid+1;
         }
-        else //ascending part
-        {
-            s=mid+1;
-        }
-        }
-        return s; //or return e
-    }
+        return s;
 };
 
 //Peak Index (sub 1095)
