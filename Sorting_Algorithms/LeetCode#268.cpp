03a5294e2fb7b7de68416b81796bbcb2bfffe2ff
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-09-04 15:48:34 +0530

    Q: 268 Missing Number

---
+++ Sorting_Algorithms/LeetCode#268.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=i)
+                return i;
+        }
+        return nums.size();
+    }
+};
