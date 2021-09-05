41b32d7631cc1001b403ce0e93949d7dfe802ef3
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-09-05 11:31:26 +0530

    Create LeetCode#645.cpp

---
+++ Sorting_Algorithms/LeetCode#645.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int>v;
+        int i=0;
+        while(i<nums.size()){
+            int corr=nums[i]-1;
+            if(nums[i]!=nums[corr]){
+                swap(nums[i],nums[corr]);
+            }else{
+                i++;
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=(i+1)){
+                v.push_back(nums[i]);
+                v.push_back(i+1);
+            }
+        }
+        return v;
+    }
+};
