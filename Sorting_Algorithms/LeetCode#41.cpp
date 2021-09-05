e868c849dd23a902ea3141f40fcaa3dc93569683
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-09-05 12:09:21 +0530

    Create LeetCode#41.cpp

---
+++ Sorting_Algorithms/LeetCode#41.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int i=0;
+        while(i<nums.size()){
+            //signed int c = ;
+            if(nums[i]>0 && nums[i]<=nums.size() && nums[i]!=nums[nums[i]-1]){
+                swap(nums[i],nums[nums[i]-1]);
+            }else{
+                i++;
+            }
+        }
+        int ans=nums.size();
+        for(int i=0;i<nums.size();i++)
+        {
+            if((nums[i]-1)!=i)
+            {
+                ans=i;
+                break;
+            }
+        }
+        return ans+1;
+    }
+};
